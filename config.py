9bb07eeb46faebb207170dc90e039a4c6ff6760b
abdullah001111 <143931418+abdullah001111@users.noreply.github.com>
2023-10-15 21:56:27 +0600

    Update config.py

--- config.py
+++ config.py
@@ -10,38 +10,35 @@ load_dotenv("config.env")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-API_ID = int(os.environ.get("API_ID", "2040"))
-API_HASH = os.environ.get("API_HASH", "b18441a1ff607e10a989891a5462e627")
+API_ID = int(os.environ.get("API_ID", "27382214"))
+API_HASH = os.environ.get("API_HASH", "6a3913eb3f026ab02e7ac1c420df2ad0")
 
-CHANNEL_DB = int(os.environ.get("CHANNEL_DB", ""))
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+CHANNEL_DB = int(os.environ.get("CHANNEL_DB", "-1001982189344"))
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ken:ken@cluster0.4dbw2ui.mongodb.net/?retryWrites=true&w=majority")
 
 RESTRICT = strtobool(os.environ.get("RESTRICT", "True"))
 
-FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "0"))
-FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "0"))
-FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
-FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
+FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001564901630"))
+FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001927957401"))
+FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "-1001930063761"))
+FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "-1001977636876"))
 
 WORKERS = int(os.environ.get("WORKERS", "4"))
 
 START_MESSAGE = os.environ.get(
     "START_MESSAGE",
-    "Halo {mention}!"
+    "Hello {mention}👋"
     "\n\n"
-    "Saya dapat menyimpan file pribadi di Channel tertentu dan pengguna lain dapat mengaksesnya dari link khusus.",
+    "I am  a file store bot for @anime_channelz, please join the channel to use me.",
 )
 FORCE_MESSAGE = os.environ.get(
     "FORCE_MESSAGE",
-    "Halo {mention}!"
+    "Sᴏʀʀy Dᴜᴅᴇ, Seems Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😁. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ to use me."
     "\n\n"
-    "Anda harus bergabung di Channel/Group terlebih dahulu untuk melihat file yang saya bagikan."
-    "\n\n"
-    "Silakan Join Ke Channel/Group terlebih dahulu.",
 )
 
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split(5984303934 1690217497 6672416716))]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
     
